4431bd54da02dcd067d9445925c15d881d831b2c
Ganeshk750 <ganesh9ot@gmail.com>
2019-07-28 13:12:02 +0530

    Second Commit

---
+++ webrequestvaiPromises.js
@@ -0,0 +1,23 @@
+function testCall(url){
+return new Promise(function(resolved, reject){
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function(){
+        if(request.status == 200){
+            resolved(request.response);
+        }else{
+            reject(Error(request.statusText));
+        }
+    }
+    request.onerror = function(){
+        reject(Error('Network Issues'));
+    }
+    request.send();
+ });
+}
+testCall('https://randomuser.me/api/')
+.then(function(response){
+    console.log('Wel Come');
+   },function(error){
+      console.log('error there');
+});
